b1fea0ed8992b1e493e349ec888c253d8d5663aa
danielwangai <mainad57@gmail.com>
2017-01-23 18:26:53 +0300

    Add dummy method to add tasks

--- app/kanban.py
+++ app/kanban.py
@@ -7,5 +7,10 @@ cursor = connect.cursor()
 # create tasks table if it doesn't exist
 cursor.execute("CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, task_name TEXT NOT NULL, status INT NOT NULL, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
 
-# close db connection
-connect.close()
+def add_task():#to-do
+    # test with dummy data
+    cursor.execute("INSERT INTO tasks(task_name, status) VALUES(?, ?)", ('task 1', 1))
+    # persist changes
+    connect.commit()
+    # close db connection
+    connect.close()
