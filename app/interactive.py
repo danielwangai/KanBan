0d784826b35e8b67b3f3d2346876472936c7e88f
danielwangai <mainad57@gmail.com>
2017-01-25 11:42:38 +0300

    Add method that moves card from todo to doing if record exists.

--- app/interactive.py
+++ app/interactive.py
@@ -10,3 +10,10 @@ def check_if_card_exists(task_id):
     cursor.execute("SELECT * FROM tasks WHERE id = ?", ([task_id]))
     result = cursor.fetchall()
     return (len(result) != 0)#returns boolean - True if list contains a fetched record
+
+def move_card_from_todo_to_doing(task_id):
+    if check_if_card_exists(task_id) == True:
+        move_todo_to_doing(task_id)
+        print( "The task of id {0} was successfully moved to DOING state.".format(task_id))
+    else:
+        return "The task of id {0} doesnt exist. Type *list_doing* to list all task in TODO state.".format(task_id)
